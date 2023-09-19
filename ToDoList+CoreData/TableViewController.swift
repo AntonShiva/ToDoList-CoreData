324ffe44dc1a4bb7112a85d3c2ba9bde0030c48c
AntonShiva <ryashinanthon@yandex.ru>
2023-09-19 17:02:12 +0300

    добавил алер для заполнения таблицы

--- ToDoList+CoreData/TableViewController.swift
+++ ToDoList+CoreData/TableViewController.swift
@@ -8,8 +8,29 @@
 import UIKit
 
 class TableViewController: UITableViewController {
+    
+    var tasks: [String] = []
 
     @IBAction func saveTask(_ sender: UIBarButtonItem) {
+
+        
+        let alertController = UIAlertController(title: "New Task", message: "Please add a new task", preferredStyle: .alert)
+        let saveAction = UIAlertAction(title: "Save", style: .default) { action in
+            let tf = alertController.textFields?.first
+            if let newTask = tf?.text {
+                self.tasks.insert(newTask, at: 0)
+                self.tableView.reloadData()
+            }
+        }
+        
+        alertController.addTextField { _ in }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .default) { _ in }
+        
+        alertController.addAction(saveAction)
+        alertController.addAction(cancelAction)
+        
+        present(alertController, animated: true, completion: nil)
+        
     }
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,23 +46,23 @@ class TableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return tasks.count
     }
 
-    /*
+   
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
 
-        // Configure the cell...
+        cell.textLabel?.text = tasks[indexPath.row]
 
         return cell
     }
-    */
+  
 
     /*
     // Override to support conditional editing of the table view.
